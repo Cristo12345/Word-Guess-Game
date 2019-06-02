689a40ee96ef169160efd8d11fcb2eda3d74d9ea
Christian Suleiman <cisuleiman@gmail.com>
2019-06-02 01:45:54 -0400

    began work on dealing with user inputs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,26 +16,48 @@ var introText = document.getElementById("intro-text");
 var winText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var wordDisplay = document.getElementById("word-text");
+var guessesRemainingText = document.getElementById("guessesRemaining-text");
 
 
-//var to choose a random word from wordBank
 document.onkeyup = function (event) {
-    introText.textContent = "Good luck!";
 
+    //initializing word to be guessed in global scope so it can be referenced in functions withour resetting it
     var word = wordBank[Math.floor(Math.random() * wordBank.length)];
-    var wordSplit = word.split("");
 
-    console.log(wordSplit);
-    //my method of creating the blank template for the word to be guessed.
-    //idea is to split the word into an array of characters. If a character is a letter, make it a _; if it's a _, make it a -
-    for (let i = 0; i < word.length; i++) {
-        if (wordSplit[i] !== " ") {
-            wordDisplay.textContent += "_ ";
-        } else if (wordSplit[i] == " ") {
-            wordDisplay.textContent += "-";
+    //User hits Enter: Starts/restarts the game, sets guesses remaining back to default as well as clearing the board and generating a new word.
+    if (event.which == 13) {
+        introText.textContent = "Good luck! Press Enter again to restart.";
+        wordDisplay.textContent = "";
+
+        //Restart Guesses left
+        guessesLeft = 10;
+        guessesRemainingText.textContent = "Guesses Left: " + guessesLeft;
+
+        //generate random word from word bank
+        var word = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+        //my method of creating the blank template for the word to be guessed.
+        //idea is to split the word into an array of characters. If a character is a letter, make it a _; if it's a _, make it a -
+        var wordSplit = word.split("");
+
+        for (let i = 0; i < word.length; i++) {
+            if (wordSplit[i] !== " ") {
+                wordDisplay.textContent += "_ ";
+            } else if (wordSplit[i] == " ") {
+                wordDisplay.textContent += "-";
+            };
         };
+    }
+
+
+    //User enters a valid character
+    else  {
+        var wordSplit = word.split("");
+
+        //if-else block dealing with correct and incorrect responses
+        if ((wordSplit.includes(event.key)) == false) {
+            guessesLeft -= 1;
+            guessesRemainingText.textContent = "Guesses Left: " + guessesLeft;
+        }
     };
 };
-
-
-console.dir(wordDisplay);
