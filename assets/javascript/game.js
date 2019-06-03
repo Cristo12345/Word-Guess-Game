24d613ea45e969dfdd5b6313bfc6b1e22f4c8ca7
Christian Suleiman <cisuleiman@gmail.com>
2019-06-03 18:52:57 -0400

    game now correctly deals with wrong inputs as well as user running out of guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,6 +22,7 @@ var wordDisplay = document.getElementById("word-text");
 var guessesRemainingText = document.getElementById("guessesRemaining-text");
 var lettersGuessedText = document.getElementById("lettersGuessed-text");
 
+
 //function to assign word to game, to be called upon page load
 function startGame() {
     word = wordBank[Math.floor(Math.random() * wordBank.length)];
@@ -32,6 +33,8 @@ function startGame() {
 
     //clear lettersGuessed array
     lettersGuessed = [];
+    lettersGuessed.textContent = "";
+    console.log("LettersGuessed:", lettersGuessed);
 
 
     //split Word into an array of single characters. For each space, put a hyphen. Else, put a '_ '
@@ -53,9 +56,10 @@ function startGame() {
 //check if user has run out of guesses. If so, end the game (with an alert for now), start a new game, and icrement Losses by 1
 function lossCheck() {
     if (guessesLeft == 0) {
-        alert("You lose!");
+        losses+=1;
+        lossesText.textContent = losses;
+        alert("You lose! The word was "+ word);
         startGame();
-        losses++;
     }
 }
 
@@ -64,43 +68,46 @@ function lossCheck() {
 
 //on page load, start function to start game - now first word is selected upon page load
 document.addEventListener("load", startGame());
-// console.log(word);
+
 
 //onkey up events after is initialized
 
 document.onkeyup = function (event) {
 
-        //User hits Enter: Starts/restarts the game
-        if (event.which == 13) {
-            startGame();
+    //User hits Enter: Starts/restarts the game
+    if (event.which == 13) {
+        startGame();
+    }
+
+
+    //User enters a character besides Enter
+    else {
+        var wordSplit = word.split("");
+
+        //if-else block dealing with correct and incorrect responses
+
+        // Case to deal with incorrect character
+        if ((wordSplit.includes(event.key)) == false) {
+
+            // nested if to check if key selected has already been entered. If not, append it to LettersGuessed. Else, 
+            if (!(lettersGuessed.includes(event.key))) {
+                guessesLeft -= 1;
+                guessesRemainingText.textContent = "Guesses Left: " + guessesLeft;
+
+                //updating list of incorrect guessed letters
+                lettersGuessed.push(event.key);
+                lettersGuessedText.textContent = lettersGuessed;
+                var lettersGuessedString = lettersGuessed.join(' ');
+                lossCheck();
+            }
         }
 
-
-        //User enters a character besides Enter
+        // Case to deal with correct character
         else {
-            var wordSplit = word.split("");
+            var charGuessed = event.key;
+            
+        }
 
-            //if-else block dealing with correct and incorrect responses
+    }
 
-            // Case to deal with incorrect character
-            if ((wordSplit.includes(event.key)) == false) {
-
-                // nested if to check if key selected has already been entered. If not, append it to LettersGuessed. Else, 
-                if (!(lettersGuessed.includes(event.key))) {
-                        guessesLeft -= 1;
-                        guessesRemainingText.textContent = "Guesses Left: " + guessesLeft;
-
-                        //updating list of incorrect guessed letters
-                        lettersGuessed.push(event.key);
-                        lettersGuessedText.textContent = lettersGuessed;
-                        var lettersGuessedString = lettersGuessed.join(' ');
-                    }}
-
-                }
-
-                // case to deal with if user enters a correct character - should also include a function here to check if user has guessed entire word
-                // else {
-
-
-                // }
-            };
+};
